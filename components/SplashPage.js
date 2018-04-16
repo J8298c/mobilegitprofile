b95105ce370aa0ac976c38e71b45638f359f20b3
j8298c <juliocmojica@gmail.com>
2018-04-15 21:29:09 -0400

    init commit with homepage component

---
+++ components/SplashPage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
+
+const Homepage = () => (
+  <View style={styles.homeContainer}>
+    <View>
+      <Text style={styles.titleContainer}>MyGit</Text>
+    </View>
+    <View>
+      <Ionicons name="logo-github" color="white" style={styles.iconStyle} />
+    </View>
+    <Button
+      title="Login"
+      raised
+      style={styles.buttonContainer}
+      backgroundColor="#536DFE"
+      large
+    />
+  </View>
+);
+
+const styles = {
+  homeContainer: {
+    flex: 1,
+    backgroundColor: '#3F51B5',
+    justifyContent: 'center'
+  },
+  titleContainer: {
+    textAlign: 'center',
+    fontSize: 45,
+    marginBottom: 100,
+    color: '#fcfcfc'
+  },
+  iconStyle: {
+    fontSize: 145,
+    marginBottom: 100,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    display: 'flex',
+    backgroundColor: '#C5CAE9'
+  }
+};
+
+export default Homepage;
